a7e43b73a293b622578bef25a9747acebb033b37
SteveDunn720 <sldunn0501@gmail.com>
2026-08-20 13:21:02 -0600

    small UE_import change

--- UE_asset_export/UE_import.py
+++ UE_asset_export/UE_import.py
@@ -2,11 +2,11 @@ import unreal
 import os
 
 # --- CONFIGURATION ---
-ASSET = 'BarrelA'
+ASSET = 'WindowA'
 # The local path where your Maya exports are located
 SOURCE_DIR = f"C:/Users/sd547/Box/Sprint2026/Mistborn/test_repo/{ASSET}" 
 # Where in the Content Browser you want them to go
-GAME_PATH = f"/Game/Assets/{ASSET}"
+GAME_PATH = f"/Game/Realtime2026/Environment/SteveDunn/StaticMeshes/{ASSET}" 
 
 EXTENSIONS = (".fbx", ".obj")
  
